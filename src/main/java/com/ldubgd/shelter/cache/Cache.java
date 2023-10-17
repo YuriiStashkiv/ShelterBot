fbc39e2e45f221ef2a2ba9e10504ee76f7d5144a
YuriiStashkiv <stashkiv.yuri@gmail.com>
2023-10-17 21:14:23 +0300

    Creating Cache interface but not sure if will use it

---
+++ src/main/java/com/ldubgd/shelter/cache/Cache.java
@@ -0,0 +1,14 @@
+package com.ldubgd.shelter.cache;
+
+import java.util.List;
+
+public interface Cache<T> {
+
+    void add(T t);
+
+    void remove(T t);
+
+    T findBy(Long id);
+
+    List<T> getAll();
+}
